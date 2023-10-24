a900dc4fcafbf0352d5eca81e5ae8c677f6e7a2d
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-10-24 14:14:13 +0800

    init setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -12,7 +16,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'black-trans-25': "rgba(0,0,0,0.25)",
+        'black-trans-35': "rgba(0,0,0,0.35)",
+        'black-trans-55': "rgba(0,0,0,0.55)",
+        'black-trans-75': "rgba(0,0,0,0.75)",
+        'black-trans-95': "rgba(0,0,0,0.95)",
+      }
     },
   },
   plugins: [],
-}
+})
