6518378c46fc5636d2cd618a663ec9d24364bd76
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-10-30 18:18:26 +0800

    feat: PUT api/report/[id] for updating report

--- app/api/report/[id]/route.js
+++ app/api/report/[id]/route.js
@@ -1,5 +1,5 @@
-import Report from "@/model/report"
-import { connectToDB } from "@/utils/database"
+import Report from "@/model/report";
+import { connectToDB } from "@/utils/database";
 
 export const GET = async (req, { params }) => {
   const { id } = params;
@@ -17,12 +17,39 @@ export const GET = async (req, { params }) => {
   } catch (error) {
     console.log(error.message);
     if (error.name === "CastError" && error.path === "_id") {
-      return new Response(
-        JSON.stringify({ error: "Invalid Report ID" }),
-        {
-          status: 400,
-        }
-      );
+      return new Response(JSON.stringify({ error: "Invalid Report ID" }), {
+        status: 400,
+      });
+    }
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+};
+
+export const PUT = async (req, { params }) => {
+  const updateData = await req.json();
+  const { id } = params;
+console.log({updateData})
+  try {
+    await connectToDB();
+
+    const report = await Report.findByIdAndUpdate(id, updateData, {
+      new: true,
+    }).select("-__v");
+
+    if (!report)
+      return new Response(JSON.stringify({ error: "Report Not Found" }), {
+        status: 404,
+      });
+
+    return new Response(JSON.stringify(report), { status: 200 });
+  } catch (error) {
+    console.error(error.message);
+    if (error.name === "CastError" && error.path === "_id") {
+      return new Response(JSON.stringify({ error: "Invalid Report ID" }), {
+        status: 400,
+      });
     }
     return new Response(JSON.stringify({ error: "Internal Server Error" }), {
       status: 500,
