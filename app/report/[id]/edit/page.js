855fe3986bcc9f32120f51bca0630ee99cc1536a
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-10-30 18:24:26 +0800

    feat: Edit report page

---
+++ app/report/[id]/edit/page.js
@@ -0,0 +1,76 @@
+"use client";
+import { useEffect, useState } from "react";
+import {
+  Card,
+  Typography,
+  ReportForm,
+  Alert,
+  Spinner,
+} from "@/components/client";
+import { getReport } from "@/services/api";
+import { useParams } from "next/navigation";
+
+import ReportRoundedIcon from "@mui/icons-material/ReportRounded";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
+
+const EditReport = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    getReport({
+      reportId: params.id,
+      onSubmit: () => setIsLoading(true),
+      onSuccess: (data) => {
+        setIsLoading(false);
+        setError(null);
+        setData(data);
+      },
+      onFailed: ({ status, data }) => {
+        setIsLoading(false);
+        setError({ status, message: data.error });
+      },
+    });
+  }, [params.id]);
+
+  return (
+    <main className="min-h-screen">
+      <Card className="lg:mt-20 mt-16 mb-4 p-10 max-w-xl sm:mx-auto mx-2">
+        <Typography variant="h4" color="blue-gray">
+          Edit Crime and Incident Report
+        </Typography>
+        <hr className="mt-8" />
+        {isLoading ? (
+          <Alert color="blue-gray" variant="ghost" className="mt-4">
+            <section className="flex gap-2">
+              <Spinner />
+              <Typography>Loading...</Typography>
+            </section>
+          </Alert>
+        ) : error ? (
+          <Alert
+            color={error.status !== 404 && "red"}
+            variant={error.status === 404 ? "gradient" : "ghost"}
+            className="mt-4"
+          >
+            <section className="flex gap-2">
+              {error.status === 404 ? (
+                <ReportRoundedIcon />
+              ) : (
+                <WarningRoundedIcon />
+              )}
+              <Typography>{error.message}</Typography>
+            </section>
+          </Alert>
+        ) : (
+          data && <ReportForm defaultFormData={data} action="PUT" />
+        )}
+      </Card>
+    </main>
+  );
+};
+
+export default EditReport;
