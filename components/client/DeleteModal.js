07d651b0f218531fd6de092057387766d4b66546
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-11-01 04:22:39 +0800

    feat: DeleteModal component

---
+++ components/client/DeleteModal.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Button,
+  Alert,
+  Typography,
+  Spinner,
+} from "@/components/client";
+
+import ReportRoundedIcon from "@mui/icons-material/ReportRounded";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
+
+const DeleteModal = ({
+  open,
+  onConfirm = () => {},
+  onCancel = () => {},
+  loading,
+  error,
+}) => {
+  return (
+    <Dialog open={open} size="xs">
+      <DialogHeader>Delete Report</DialogHeader>
+      <DialogBody className="flex flex-col gap-2">
+        <section>
+          Are you sure you want to delete this report? This action cannot be
+          undone.
+        </section>
+        <section>
+          {error && (
+            <Alert
+              color={error.status !== 404 && "red"}
+              variant="ghost"
+              className="mt-4"
+            >
+              <section className="flex gap-2">
+                {error.status === 404 ? (
+                  <ReportRoundedIcon />
+                ) : (
+                  <WarningRoundedIcon />
+                )}
+                <Typography>{error.message}</Typography>
+              </section>
+            </Alert>
+          )}
+        </section>
+      </DialogBody>
+      <DialogFooter>
+        <Button
+          variant="text"
+          size="lg"
+          color="gray"
+          onClick={onCancel}
+          className="mr-1 border"
+          disabled={loading}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="gradient"
+          size="lg"
+          color="red"
+          onClick={onConfirm}
+          disabled={loading}
+          className="flex gap-1 items-center"
+        >
+          {loading && <Spinner color="red" />}
+          Delete
+        </Button>
+      </DialogFooter>
+    </Dialog>
+  );
+};
+
+export default DeleteModal;
