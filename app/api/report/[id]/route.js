bb82c50c02c0462b4ee2111d802c7d975e9c4a11
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-11-01 04:20:55 +0800

    feat: DELETE api/report/[id] for deleting one report

--- app/api/report/[id]/route.js
+++ app/api/report/[id]/route.js
@@ -56,3 +56,31 @@ console.log({updateData})
     });
   }
 };
+
+export const DELETE = async (req, { params }) => {
+  const { id } = params;
+
+  try {
+    await connectToDB();
+
+    const report = await Report.deleteOne({ _id: id});
+    // {"acknowledged":true,"deletedCount":1}
+
+    if (!report || report.deletedCount <= 0)
+      return new Response(JSON.stringify({ error: "No File Deleted! Report Not Found" }), {
+        status: 404,
+      });
+
+    return new Response(JSON.stringify(report), { status: 200 });
+  } catch (error) {
+    console.log(error.message);
+    if (error.name === "CastError" && error.path === "_id") {
+      return new Response(JSON.stringify({ error: "Invalid Report ID" }), {
+        status: 400,
+      });
+    }
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+};
