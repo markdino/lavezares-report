621b7c9f621b88ab7b46513727eae4a4350dc3d1
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-11-11 18:43:10 +0800

    feat: isValidEmail util function

--- utils/validation.js
+++ utils/validation.js
@@ -2,3 +2,8 @@ export const isVideo = (inputString) => {
     const videoExtensions = ['.mp4', '.webm', '.ogg', '.mov', '.avi', '.flv', '.mkv'];
     return videoExtensions.some(ext => inputString.endsWith(ext));
 }
+
+export const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
