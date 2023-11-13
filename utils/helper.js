2f9b24c4959bf80faa28b5d05bcadb6d9b623b68
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-11-13 21:38:32 +0800

    fix: Update extractFields to also handle Mongoose obj

--- utils/helper.js
+++ utils/helper.js
@@ -1,13 +1,13 @@
 export const extractFields = (obj, fields) => {
-  if (typeof obj !== "object" || obj === null) {
-    throw new Error("The first parameter must be an object.");
+  if (!obj || typeof obj.toObject !== 'function') {
+    throw new Error('Invalid object provided.');
   }
 
-  const fieldArray = fields.split(" ");
+  const fieldArray = fields.split(' ');
   const newObj = {};
 
-  fieldArray.forEach((field) => {
-    if (obj.hasOwnProperty(field)) {
+  fieldArray.forEach(field => {
+    if (obj.toObject().hasOwnProperty(field)) {
       newObj[field] = obj[field];
     }
   });
