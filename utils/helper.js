f9866e6584e6f2f0bfda289b76dfbd37e34b42be
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-11-18 04:23:38 +0800

    feat: removeEmptyProperties helper function for Obj

--- utils/helper.js
+++ utils/helper.js
@@ -1,18 +1,30 @@
 export const extractFields = (obj, fields) => {
-  if (obj && typeof obj.toObject === 'function') {
+  if (obj && typeof obj.toObject === "function") {
     obj = obj.toObject();
-  } else if (typeof obj !== 'object' || obj === null) {
-    throw new Error('Invalid object provided.');
+  } else if (typeof obj !== "object" || obj === null) {
+    throw new Error("Invalid object provided.");
   }
 
-  const fieldArray = fields.split(' ');
+  const fieldArray = fields.split(" ");
   const newObj = {};
 
-  fieldArray.forEach(field => {
+  fieldArray.forEach((field) => {
     if (obj.hasOwnProperty(field)) {
       newObj[field] = obj[field];
     }
   });
+
+  return newObj;
+};
+
+export const removeEmptyProperties = (obj) => {
+  const newObj = {};
+
+  for (let key in obj) {
+    if (obj[key] !== "" && obj[key] !== null && obj[key] !== undefined) {
+      newObj[key] = obj[key];
+    }
+  }
 
   return newObj;
 };
