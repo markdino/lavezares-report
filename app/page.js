f709e9d6a01e7c22eb3b63cc2ff88e161246f7a6
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2024-01-31 03:19:52 +0800

    feat: SafetyInfoSection

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import Hero from "@/components/Hero";
+import SafetyInfoSection from "@/components/SafetyInfoSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
-      <div className="max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello</h1>
-      </div>
+      <SafetyInfoSection />
     </main>
   );
 }
---
+++ components/SafetyInfoSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SafetyInfoSection = () => {
+  return (
+    <section className="w-full py-28 px-10">
+      <section className="text-center max-w-5xl mx-auto">
+        <h2 className="font-bold lg:text-5xl text-4xl text-center mb-4">
+          HELP YOURSELF, YOUR FRIENDS AND YOUR FAMILY
+        </h2>
+        <p>
+          Reporting a crime is an important part of keeping our communities
+          safe. By reporting a crime, you can help law enforcement to identify,
+          investigate, and prosecute criminals. You can also help to protect
+          yourself, your family, and your neighbors from becoming victims of
+          crime. Reporting a crime also allows law enforcement to track and
+          analyze crime trends, which can help them to be more effective in
+          preventing and solving future crimes. Finally, by reporting a crime,
+          you can help to ensure justice is served and that the perpetrators of
+          the crime are held accountable.
+        </p>
+      </section>
+    </section>
+  );
+};
+
+export default SafetyInfoSection;
