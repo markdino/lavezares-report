fc2c02fd233c909d404eff3ba98b09ee2551c5a5
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2024-02-08 23:39:56 +0800

    feat: Authenticate user access on api/report/[id]

--- app/api/report/[id]/route.js
+++ app/api/report/[id]/route.js
@@ -1,8 +1,22 @@
 import Report from "@/model/report";
 import { connectToDB } from "@/utils/database";
 import { utapi } from "../../uploadthing/core";
+import { AUTH_TOKEN } from "@/utils/constant";
+import { cookies } from "next/headers";
+import { verify } from "jsonwebtoken";
+
+const secret = process.env.JWT_KEY || "";
 
 export const GET = async (req, { params }) => {
+  const cookieStore = cookies();
+  const authToken = cookieStore.get(AUTH_TOKEN)?.value;
+
+  if (!authToken) {
+    return new Response(JSON.stringify({ error: "Unauthrorized user" }), {
+      status: 401,
+    });
+  }
+
   const { id } = params;
 
   try {
@@ -13,6 +27,20 @@ export const GET = async (req, { params }) => {
       return new Response(JSON.stringify({ error: "Report Not Found" }), {
         status: 404,
       });
+
+    const { isAdmin, _id } = verify(authToken, secret);
+
+    if (report.creator && !isAdmin) {
+      if (report.creator.toString() !== _id)
+        return new Response(
+          JSON.stringify({
+            error: "Access limited to data owner or admin users only",
+          }),
+          {
+            status: 403,
+          }
+        );
+    }
 
     return new Response(JSON.stringify(report), { status: 201 });
   } catch (error) {
@@ -29,20 +57,43 @@ export const GET = async (req, { params }) => {
 };
 
 export const PUT = async (req, { params }) => {
-  const updateData = await req.json();
+  const cookieStore = cookies();
+  const authToken = cookieStore.get(AUTH_TOKEN)?.value;
+
+  if (!authToken) {
+    return new Response(JSON.stringify({ error: "Unauthrorized user" }), {
+      status: 401,
+    });
+  }
+
   const { id } = params;
 
   try {
     await connectToDB();
 
-    const report = await Report.findByIdAndUpdate(id, updateData, {
-      new: true,
-    }).select("-__v");
+    const report = await Report.findById(id);
 
     if (!report)
       return new Response(JSON.stringify({ error: "Report Not Found" }), {
         status: 404,
       });
+
+    const { _id } = verify(authToken, secret);
+
+    if (!report.creator || report.creator?.toString() !== _id)
+      return new Response(
+        JSON.stringify({
+          error: "Access limited to data owner only",
+        }),
+        {
+          status: 403,
+        }
+      );
+
+    const updateData = await req.json();
+
+    report.overwrite(updateData);
+    await report.save();
 
     return new Response(JSON.stringify(report), { status: 200 });
   } catch (error) {
@@ -59,6 +110,15 @@ export const PUT = async (req, { params }) => {
 };
 
 export const DELETE = async (req, { params }) => {
+  const cookieStore = cookies();
+  const authToken = cookieStore.get(AUTH_TOKEN)?.value;
+
+  if (!authToken) {
+    return new Response(JSON.stringify({ error: "Unauthrorized user" }), {
+      status: 401,
+    });
+  }
+
   const { id } = params;
 
   try {
@@ -73,7 +133,21 @@ export const DELETE = async (req, { params }) => {
         }
       );
 
-      // If report has a file(s), delete it from UTAPI server
+    const { isAdmin, _id } = verify(authToken, secret);
+
+    if (report.creator && !isAdmin) {
+      if (report.creator.toString() !== _id)
+        return new Response(
+          JSON.stringify({
+            error: "Access limited to data owner or admin users only",
+          }),
+          {
+            status: 403,
+          }
+        );
+    }
+
+    // If report has a file(s), delete it from UTAPI server
     if (report.files?.length > 0) {
       await utapi.deleteFiles(report.files.map((file) => file.key));
     }
